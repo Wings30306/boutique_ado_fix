4f1f3dd3a2fe7be7480913d8d4e81e989d01973a
AJGreaves <anna.j.greaves@gmail.com>
2025-01-30 16:10:43 +0100

    Handling Payment Intents and Securing Keys with Environment Variables

---
+++ example_env.py
@@ -0,0 +1,8 @@
+""" 
+This is an example file of the environment variables you should 
+have set in the env.py file of your project to run it locally.
+"""
+import os
+
+os.environ.setdefault('STRIPE_PUBLIC_KEY', 'your stripe publishable key, get this from your stripe developers dashboard')
+os.environ.setdefault('STRIPE_SECRET_KEY', 'your stripe secret key, get this from your stripe developers dashboard')
