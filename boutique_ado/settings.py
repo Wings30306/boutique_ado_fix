a8a175d5b9736056457894b4771ad889e8ab645a
AJGreaves <anna.j.greaves@gmail.com>
2025-01-31 14:08:33 +0100

    Add cache checkout data view and enhance Stripe payment handling

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost' # listen for stripe webhooks
+    '127.0.0.1', # local host
+    'localhost', # listen for stripe webhooks
 ]
 
 
