a98aca5e73ad48550bbb134f2d5e30059d8b78e8
AJGreaves <anna.j.greaves@gmail.com>
2025-01-31 09:41:26 +0100

    Add Stripe webhook secret configuration and update allowed hosts.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-bf_nx7m0sd(kbofoqv1g*ba))$ux)2(ju#mr-kr5#21@5iql_d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost' # listen for stripe webhooks
+]
 
 
 # Application definition
