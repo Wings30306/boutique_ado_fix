6a791e1be3c24d195bbd6a34eb59b321a31af76c
AJGreaves <anna.j.greaves@gmail.com>
2025-02-03 14:58:40 +0100

    Add configuration for production database.

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import os
+import dj_database_url
 from pathlib import Path
 if os.path.isfile('env.py'):
     import env
@@ -131,7 +133,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
