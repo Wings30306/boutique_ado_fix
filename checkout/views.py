20db50f1501ffd253a774b110ebb1771a58ba075
AJGreaves <anna.j.greaves@gmail.com>
2025-02-03 12:45:35 +0100

    Enhance profile update functionality in webhook handler.

--- checkout/views.py
+++ checkout/views.py
@@ -53,7 +53,7 @@ def checkout(request):
         if order_form.is_valid():
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
-            order.strip_pid = pid
+            order.stripe_pid = pid
             order.original_bag = json.dumps(bag)
             order.save()
             for item_id, item_data in bag.items():
